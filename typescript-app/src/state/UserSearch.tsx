16163b6931e550ba7e4f8e6b44bc6e65595714af
gabrielc42 <clark.gabe97@gmail.com>
2022-11-08 11:04:50 -0600

    user search, correct index.tsx

--- typescript-app/src/state/UserSearch.tsx
+++ typescript-app/src/state/UserSearch.tsx
@@ -8,15 +8,26 @@ const users = [
 
 const UserSearch: React.FC = () => {
   const [name, setName] = useState('');
+  const [user, setUser] = useState<{name: string, age: number} | undefined>();
 
-  const onClick = () => {};
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+    setUser(foundUser)
+  };
 
   return (
     <div>
       User Search
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
     </div>
+
   );
 };
 
