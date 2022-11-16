46d2ebb33b5aa0afdca6a1e5e70a3ebed36dd6d4
gabrielc42 <clark.gabe97@gmail.com>
2022-11-16 10:59:21 -0600

    error messages displayed, reset iframe srcdoc before each bundle

--- jbook/jbook/src/index.tsx
+++ jbook/jbook/src/index.tsx
@@ -9,7 +9,6 @@ const App = () => {
     const ref = useRef<any>();
     const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -25,6 +24,8 @@ const App = () => {
         if (!ref.current) {
             return;
         }
+
+        iframe.current.srcdoc = html;
 
         const result = await ref.current.build({
             entryPoints: ['index.js'],
@@ -64,7 +65,13 @@ const App = () => {
                 <div id="root"></div>
                 <script>
                     window.addEventListener('message', (event) => {
-                        eval(event.data);
+                        try {
+                            eval(event.data);
+                        } catch (err) {
+                            const root = document.querySelector('#root');
+                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                            console.error(err);
+                        }
                     }, false);
                 </script>
             </body>
@@ -79,8 +86,7 @@ const App = () => {
         <div>
             <button onClick={onClick}>Submit</button>
         </div>
-        <pre>{code}</pre>
-        <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+        <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
 };
 
