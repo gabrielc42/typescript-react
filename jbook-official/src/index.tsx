ab22435a44f7dad6ee7defc5342d084016a19a8f
gabrielc42 <clark.gabe97@gmail.com>
2022-11-17 11:16:27 -0600

    jbook2 success

---
+++ jbook-official/src/index.tsx
@@ -0,0 +1,58 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
+
+const App = () => {
+  const ref = useRef<any>();
+  const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <Preview code={code} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
--- jbook2/jbook/src/index.tsx
+++ jbook2/jbook/src/index.tsx
@@ -2,46 +2,29 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { useState, useEffect, useRef } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import bundle from './bundler';
 
 const App = () => {
-    const ref = useRef<any>();
+    // const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        });
-    };
-    useEffect(() => {
-        startService();
-    }, []);
+    // const startService = async () => {
+    //     ref.current = 
+    // };
+    // useEffect(() => {
+    //     startService();
+    // }, []);
 
     const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
+        const output = await bundle(input);
+        // if (!ref.current) {
+        //     return;
+        // }
 
-
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [
-                unpkgPathPlugin(),
-                fetchPlugin(input)
-            ],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window',
-            },
-        });
+        // const result = 
 
         // const result = await ref.current.transform(input, {
         //     loader: 'jsx',
@@ -49,7 +32,7 @@ const App = () => {
         // });
 
         // setCode(result.code);
-        setCode(result.outputFiles[0].text);
+        setCode(output);
 
         // try {
         //     eval(result.outputFiles[0].text);
