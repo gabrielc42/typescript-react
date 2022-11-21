d4d87eef840e5f8b7a244083ecca79d06e018d5b
gabrielc42 <clark.gabe97@gmail.com>
2022-11-21 15:08:57 -0600

    bundles react wire, adjust reducer state

--- jbook2/jbook/src/components/code-cell.tsx
+++ jbook2/jbook/src/components/code-cell.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
-import bundle from '../bundler';
 import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 interface CodeCellProps {
   cell: Cell
@@ -12,11 +12,11 @@ interface CodeCellProps {
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
-  const { updateCell } = useActions();
-
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
   // const [input, setInput] = useState('');
-  const [err, setErr] = useState('');
-  const [code, setCode] = useState('');
+  // const [err, setErr] = useState('');
+  // const [code, setCode] = useState('');
   // const ref = useRef<any>();
 
   // const startService = async () => {
@@ -28,15 +28,16 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(cell.content);
-      setCode(output.code);
-      setErr(output.err);
+      // const output = await bundle(cell.content);
+      // setCode(output.code);
+      // setErr(output.err);
+      createBundle(cell.id, cell.content)
     }, 2000);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content]);
+  }, [cell.content, cell.id]);
 
   // const onClick = async () => {
   // const output = await bundle(input);
@@ -74,9 +75,9 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         {/* <div>
           <button onClick={onClick}>Submit</button>
         </div> */}
-        <Preview
-          code={code}
-          err={err} />
+        {bundle && <Preview
+          code={bundle.code}
+          err={bundle.err} />}
       </div>
     </Resizable>
   )
