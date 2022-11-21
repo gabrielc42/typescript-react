277c8492b20e9fd93095897cb374e3aa0a1152cb
gabrielc42 <clark.gabe97@gmail.com>
2022-11-21 09:55:22 -0600

    useActions for state, default message for text editor

--- jbook2/jbook/src/components/text-editor.tsx
+++ jbook2/jbook/src/components/text-editor.tsx
@@ -1,11 +1,18 @@
 import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 import './text-editor.css';
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
 
-const TextEditor: React.FC = () => {
+interface TextEditorProps {
+  cell: Cell;
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState('# Header \n - click here :)');
+  // const [value, setValue] = useState('# Header \n - click here :)');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -26,7 +33,7 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+        <MDEditor value={cell.content} onChange={(v) => updateCell(cell.id, v || '')} />
       </div>
     )
   }
@@ -34,7 +41,7 @@ const TextEditor: React.FC = () => {
   return (
     <div className="text-editor card" onClick={() => setEditing(true)}>
       <div className="card-content">
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content || 'click here :)'} />
       </div>
     </div>
   );
