c4321e34ac14865de0735c1fe2120cee10b5f24f
gabrielc42 <clark.gabe97@gmail.com>
2022-11-23 00:06:32 -0600

    calc(100%-10px) does not work. needs spaces: 'calc(100% - 10px)'. causes code cell pulldown problems

--- jbook2/jbook/src/components/code-cell.tsx
+++ jbook2/jbook/src/components/code-cell.tsx
@@ -9,7 +9,7 @@ import { useTypedSelector } from '../hooks/use-typed-selector';
 import { useCumulativeCode } from '../hooks/use-cumulative-code';
 
 interface CodeCellProps {
-  cell: Cell
+  cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
@@ -40,7 +40,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
       // setCode(output.code);
       // setErr(output.err);
       createBundle(cell.id, cumulativeCode);
-    }, 2000);
+    }, 750);
 
     return () => {
       clearTimeout(timer);
@@ -48,59 +48,34 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cumulativeCode, cell.id, createBundle]);
 
-  // const onClick = async () => {
-  // const output = await bundle(input);
-  // if (!ref.current) {
-  //     return;
-  // }
-
-  // const result = 
-
-  // const result = await ref.current.transform(input, {
-  //     loader: 'jsx',
-  //     target: 'es2015'
-  // });
-
-  // setCode(result.code);
-  // setCode(output);
-
-  // try {
-  //     eval(result.outputFiles[0].text);
-  // } catch (err) {
-  //     alert(err);
-  // }
-  // };
 
   return (
-    <Resizable direction='vertical'>
-      <div style={{ height: 'calc(100%-10px)', display: 'flex', flexDirection: 'row' }}>
-        <Resizable direction='horizontal'>
-
+    <Resizable direction="vertical">
+      <div style={{
+        height: 'calc(100% - 10px)', display: 'flex', flexDirection: 'row',
+      }}>
+        <Resizable direction="horizontal">
           <CodeEditor
             initialValue={cell.content}
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
         <div className="progress-wrapper">
-          {/* <div>
-          <button onClick={onClick}>Submit</button>
-        </div> */}
-          {
-            !bundle || bundle.loading
-              ?
-              <div className="progress-cover">
-                <progress className="progress is-small is-primary" max="100">
-                  Loading
-                </progress>
-              </div>
-              : <Preview
-                code={bundle.code}
-                err={bundle.err} />
-          }
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview
+              code={bundle.code}
+              err={bundle.err} />
+          )}
         </div>
-      </div>
-    </Resizable>
-  )
+      </div >
+    </Resizable >
+  );
 };
 
 // const html = `
--- jbook2/jbook/src/components/preview.tsx
+++ jbook2/jbook/src/components/preview.tsx
@@ -1,5 +1,5 @@
 import './preview.css';
-import { useEffect, useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 interface PreviewProps {
     code: string;
@@ -7,33 +7,35 @@ interface PreviewProps {
 }
 
 const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    const handleError = (err) => {
-                        const root = document.querySelector('#root');
-                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-                        console.error(err);
-                    };
+    <html>
+      <head>
+        <style>html { background-color: white; }</style>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          const handleError = (err) => {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          };
 
-                    window.addEventListener('error', (event) => {
-                        event.preventDefault();
-                        handleError(event.error);
-                    });
+          window.addEventListener('error', (event) => {
+            event.preventDefault();
+            handleError(event.error);
+          });
 
-                    window.addEventListener('message', (event) => {
-                        try {
-                            eval(event.data);
-                        } catch (err) {
-                            handleError(err);
-                        }
-                    }, false);
-                </script>
-            </body>
-        </html>
-    `;
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              handleError(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
 const Preview: React.FC<PreviewProps> = ({ code, err }) => {
     const iframe = useRef<any>();
@@ -45,14 +47,17 @@ const Preview: React.FC<PreviewProps> = ({ code, err }) => {
         }, 50);
     }, [code]);
 
-    return <div className="preview-wrapper">
-        <iframe
-            title="preview"
-            ref={iframe}
-            sandbox="allow-scripts"
-            srcDoc={html} />
-        {err && <div className="preview-error">{err}</div>}
-    </div>
+    return (
+        <div className="preview-wrapper">
+            <iframe
+                title="preview"
+                ref={iframe}
+                sandbox="allow-scripts"
+                srcDoc={html}
+            />
+            {err && <div className="preview-error">{err}</div>}
+        </div>
+    );
 };
 
 export default Preview;
