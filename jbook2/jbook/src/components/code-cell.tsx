c1220ec44cab12d814b6281f4cc19aa140179fd2
gabrielc42 <clark.gabe97@gmail.com>
2022-11-22 23:29:48 -0600

    cumulative code splice in showFunc and ShowFuncNoop

--- jbook2/jbook/src/components/code-cell.tsx
+++ jbook2/jbook/src/components/code-cell.tsx
@@ -19,25 +19,30 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [
-      `
-      import _React from 'react';
-      import _ReactDOM from 'react-dom';
-        const show = (value) => {
-          const root = document.querySelector('#root');
-          if (typeof value === 'object') {
-            if (value.$$typeof && value.props) {
-              _ReactDOM.render(value, root);
-            }
-            root.innerHTML = JSON.stringify(value);
-          } else {
-            root.innerHTML = value;
+    const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+      const show = (value) => {
+        const root = document.querySelector('#root');
+        if (typeof value === 'object') {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root);
           }
-        };
-      `
-    ];
+          root.innerHTML = JSON.stringify(value);
+        } else {
+          root.innerHTML = value;
+        }
+      };
+    `;
+    const showFuncNoop = 'var show = () => {}';
+    const cumulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
