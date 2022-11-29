b1b6cc2e79bacb60ad7f6d30435ac7ecd3af22f7
gabrielc42 <clark.gabe97@gmail.com>
2022-11-28 19:41:04 -0600

    cli creation, had to change many of the dist/files for each folder

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -1,8 +1,12 @@
+import path from 'path';
 import { Command } from 'commander';
+import { serve } from 'local-api';
 
 export const serveCommand = new Command()
-  .command('serve')
+  .command('serve [filename]')
   .description('Open a file for editing')
-  .action(() => {
-    console.log('Getting ready to serve a file');
+  .option('-p, --port <number>', 'port to run server on', '4005')
+  .action((filename = 'notebook.js', options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
