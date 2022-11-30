f42a3a5e3248d0f547cfc63e524e0fc59eba46b5
gabrielc42 <clark.gabe97@gmail.com>
2022-11-29 19:14:49 -0600

    problem with memo dispatch, reorganizing and accessing older commits

---
+++ jbook-incomplete2/packages/local-api/src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+export const serve = (port: number, filename: string, dir: string) => {
+  const app = express();
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  });
+};
--- jbook/packages/local-client/src/hooks/use-actions.ts
+++ jbook/packages/local-client/src/hooks/use-actions.ts
@@ -1,7 +1,8 @@
-import { useMemo } from 'react';
+import { actionCreators } from '../state';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { actionCreators } from '../state';
+import { useMemo } from 'react';
+
 
 export const useActions = () => {
   const dispatch = useDispatch();
