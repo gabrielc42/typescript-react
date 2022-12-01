734fe5804ec3795160f03e5796dcc82f01d86442
gabrielc42 <clark.gabe97@gmail.com>
2022-11-30 21:39:46 -0600

    absolute path

--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.use(express.static('../../local-client/build'));
+  const packagePath = require.resolve('local-client/build/index.html');
+  app.use(express.static(path.dirname(packagePath)));
   // app.use(createProxyMiddleware({
   //   target: 'http://127.0.0.1:3000',
   //   ws: true,
