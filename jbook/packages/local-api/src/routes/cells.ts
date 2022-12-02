525b3706bee49a5a89bd1d27ba3c148d1b757e3e
gabrielc42 <clark.gabe97@gmail.com>
2022-12-01 21:08:44 -0600

    adding a json parser

--- jbook/packages/local-api/src/routes/cells.ts
+++ jbook/packages/local-api/src/routes/cells.ts
@@ -14,7 +14,7 @@ interface Cell {
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
-
+  router.use(express.json());
   const fullPath = path.join(dir, filename);
 
   router.get('/cells', async (req, res) => {
