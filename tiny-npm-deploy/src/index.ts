5844bd1ad08d89ec5e0fe0ae1477de825415f606
gabrielc42 <clark.gabe97@gmail.com>
2022-12-02 12:24:57 -0600

    tiny npm deploy boostrap

---
+++ tiny-npm-deploy/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi there!');
+});
+
+app.listen(3005, () => {
+  console.log('Listening on 3005');
+});
